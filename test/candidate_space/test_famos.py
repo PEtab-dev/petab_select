134e7fde11d12f088568f61346fabc4163d8939d
Doresic <domagoj.doresic@gmail.com>
2022-08-16 12:49:54 +0200

    Update test_famos.py to be compatible with ui_method change

--- test/candidate_space/test_famos.py
+++ test/candidate_space/test_famos.py
@@ -106,30 +106,26 @@ def test_famos(
     history = {}
     progress_list = []
 
-    lateral_method_switching = {
-        (Method.BACKWARD, Method.FORWARD): Method.LATERAL,
-        (Method.FORWARD, Method.BACKWARD): Method.LATERAL,
-        (Method.BACKWARD, Method.LATERAL): None,
-        (Method.FORWARD, Method.LATERAL): None,
-        (Method.FORWARD,): Method.BACKWARD,
-        (Method.BACKWARD,): Method.FORWARD,
-        (Method.LATERAL,): Method.FORWARD,
-        None: Method.LATERAL,
-    }
 
     candidate_space = petab_select_problem.new_candidate_space()
 
     try:
         predecessor_model = candidate_space.predecessor_model
         while True:
-            # Calibrated models in this iteration that improve on the predecessor
-            # model.
-            better_models = []
-            # All calibrated models in this iteration (see second return value).
-            local_history = {}
+            # Save predecessor_models and find new candidates
+            previous_predecessor_model = candidate_space.predecessor_model
+            candidate_models, history, _ = petab_select.ui.candidates(
+                problem=petab_select_problem,
+                candidate_space=candidate_space,
+                excluded_model_hashes=list(history),
+                previous_predecessor_model=predecessor_model,
+                history=history,
+            )
+            predecessor_model = candidate_space.predecessor_model
 
+            # Prepare indicies to write to progress_list
             previous_predecessor_model_parameters = (
-                candidate_space.predecessor_model.get_parameter_values(
+                previous_predecessor_model.get_parameter_values(
                     parameter_ids=predecessor_model.petab_parameters
                 )
             )
@@ -149,68 +145,29 @@ def test_famos(
                 if predecessor_model_parameters[index] == "estimate"
             ]
 
-            candidate_models, _, _ = petab_select.ui.candidates(
-                problem=petab_select_problem,
-                candidate_space=candidate_space,
-                excluded_model_hashes=list(history),
-                previous_predecessor_model=predecessor_model,
-                history=history,
-            )
-            progress_list.append(
-                (
-                    candidate_space.inner_candidate_space.method,
-                    set(
-                        previous_predecessor_model_parameter_indices
-                    ).symmetric_difference(
-                        set(predecessor_model_parameter_indices)
-                    ),
-                )
-            )
-
-            if not candidate_models:
-                raise StopIteration("No valid models found.")
-
+            # Calibrate candidate_models
             for candidate_model in candidate_models:
                 # set model_id to M_010101010101010 form
                 set_model_id(candidate_model)
                 # run calibration
                 calibrate(candidate_model, history=history)
-
-                local_history[candidate_model.model_id] = candidate_model
-                # if candidate model has better criteria, add to better models
-                if default_compare(
-                    model0=predecessor_model,
-                    model1=candidate_model,
-                    criterion=petab_select_problem.criterion,
-                ):
-                    better_models.append(candidate_model)
-
-            history.update(local_history)
-            best_model = None
-            if better_models:
-                best_model = petab_select.ui.best(
-                    problem=petab_select_problem,
-                    models=better_models,
-                    criterion=petab_select_problem.criterion,
+            # Write the progress_list for this step
+            if not candidate_space.jumped_to_most_distant:
+                progress_list.append(
+                    (
+                        candidate_space.inner_candidate_space.method,
+                        set(
+                            previous_predecessor_model_parameter_indices
+                        ).symmetric_difference(
+                            set(predecessor_model_parameter_indices)
+                        ),
+                    )
                 )
-            jumped_to_most_distant = candidate_space.update_after_calibration(
-                history=history,
-                local_history=local_history,
-                criterion=petab_select_problem.criterion,
-            )
-            # If candidate space not Famos then ignored.
-            # Else, in case we jumped to most distant in this iteration, update the
-            # best_model to the predecessor_model (jumped to model) so it becomes
-            # the predecessor model in next iteration.
-            # Also update the local_history with it.
-            if jumped_to_most_distant:
-                best_model = candidate_space.predecessor_model
-                set_model_id(best_model)
-                calibrate(best_model, history=history)
-                local_history[best_model.model_id] = best_model
-                progress_list.append(best_model.model_id)
-            if best_model is not None:
-                predecessor_model = best_model
+            else:
+                progress_list.append(predecessor_model.model_id)
+            # Stop iteration if there are no candidate_models
+            if not candidate_models:
+                raise StopIteration("No valid models found.")
 
     except StopIteration:
         assert progress_list == expected_progress_list
