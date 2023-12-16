7b6e7af8ed3cf7a39825be6b2510b5cb89b1b6e9
dilpath <dilan.private+github@outlook.com>
2023-12-16 23:12:13 +0100

    remove misc from __init__

--- petab_select/__init__.py
+++ petab_select/__init__.py
@@ -5,7 +5,6 @@ import sys
 from .candidate_space import *
 from .constants import *
 from .criteria import *
-from .misc import *
 from .model import *
 from .model_space import *
 from .model_subspace import *
