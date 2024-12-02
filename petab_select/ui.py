2926e6cde93a010afb6ddbb6184356293ef3c564
Dilan Pathirana <59329744+dilpath@users.noreply.github.com>
2024-12-02 15:32:53 +0100

    Update petab_select/ui.py
    Co-authored-by: Daniel Weindl <dweindl@users.noreply.github.com>

--- petab_select/ui.py
+++ petab_select/ui.py
@@ -97,7 +97,7 @@ def start_iteration(
     if isinstance(user_calibrated_models, dict):
         warnings.warn(
             (
-                "`calibrated_models` should be a `petab_select.Models` object."
+                "`calibrated_models` should be a `petab_select.Models` object. "
                 "e.g. `calibrated_models = "
                 "petab_select.Models(old_calibrated_models.values())`."
             ),
