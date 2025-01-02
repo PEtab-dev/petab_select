e7952198e83e658ea8fe8969fe7adf20bdcb682c
dilpath <59329744+dilpath@users.noreply.github.com>
2025-01-02 23:30:17 +0100

    fix test_problem.py path

--- test/problem/test_problem.py
+++ test/problem/test_problem.py
@@ -16,7 +16,7 @@ problem_yaml = (
 def test_round_trip():
     """Test storing/loading of a full problem."""
     problem0 = petab_select.Problem.from_yaml(problem_yaml)
-    problem0.save("output")
+    problem0.save(test_path / "output")
 
     with open(test_path / "expected_output/petab_select_problem.yaml") as f:
         problem_yaml0 = f.read()
@@ -28,7 +28,7 @@ def test_round_trip():
     with open(test_path / "output/model_space.tsv") as f:
         model_space_tsv1 = f.read()
 
-    # The the exported problem YAML is as expected, with updated relative paths.
+    # The exported problem YAML is as expected, with updated relative paths.
     assert problem_yaml1 == problem_yaml0
-    # The the exported model space TSV is as expected, with updated relative paths.
+    # The exported model space TSV is as expected, with updated relative paths.
     assert model_space_tsv1 == model_space_tsv0
