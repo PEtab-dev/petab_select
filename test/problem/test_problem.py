c0d999575aa2e54b4a9d1655727480fdd83c381a
dilpath <59329744+dilpath@users.noreply.github.com>
2025-01-02 23:20:28 +0100

    fix test_problem paths

--- test/problem/test_problem.py
+++ test/problem/test_problem.py
@@ -2,8 +2,10 @@ from pathlib import Path
 
 import petab_select
 
+test_path = Path(__file__).parent
+
 problem_yaml = (
-    Path(__file__).parents[2]
+    test_path.parent.parent
     / "doc"
     / "examples"
     / "model_selection"
@@ -16,16 +18,14 @@ def test_round_trip():
     problem0 = petab_select.Problem.from_yaml(problem_yaml)
     problem0.save("output")
 
-    with open(
-        Path(__file__) / "expected_output/petab_select_problem.yaml"
-    ) as f:
+    with open(test_path / "expected_output/petab_select_problem.yaml") as f:
         problem_yaml0 = f.read()
-    with open(Path(__file__) / "expected_output/model_space.tsv") as f:
+    with open(test_path / "expected_output/model_space.tsv") as f:
         model_space_tsv0 = f.read()
 
-    with open(Path(__file__) / "output/petab_select_problem.yaml") as f:
+    with open(test_path / "output/petab_select_problem.yaml") as f:
         problem_yaml1 = f.read()
-    with open(Path(__file__) / "output/model_space.tsv") as f:
+    with open(test_path / "output/model_space.tsv") as f:
         model_space_tsv1 = f.read()
 
     # The the exported problem YAML is as expected, with updated relative paths.
